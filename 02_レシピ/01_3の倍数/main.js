a7f8c8be0555486400f8ea46992dbfdf885ce937
MariKojima <pfkojimari@gmail.com>
2021-07-03 00:13:39 +0900

    関数の最初の部分を学習しました

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -5,3 +5,26 @@ if (number % 3 === 0) {
 }else {
     console.log(number)
 }
+
+const number = 24
+
+for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      console.log(n + "!!!!!!!")
+    } else {
+      console.log(n)
+    }
+  }
+
+  const genkiFunction(100) = function(number) {
+
+    for (let n = 1; n <= number; n++) {
+        if (n % 3 === 0) {
+          console.log(`${n}!!!!!!!`)
+        } else {
+          console.log(n)
+        }
+      }
+    }
+
+
