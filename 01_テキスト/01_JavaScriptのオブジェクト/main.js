fe649a00c2747d2c6b5d7ccf8fd540d4ca3f6e4c
MariKojima <pfkojimari@gmail.com>
2021-07-02 23:55:43 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -14,3 +14,11 @@ const hikakin = {
 
 // 練習問題
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
+
+console.dir(document)
+console.dir(window)
+window.console.log("hello")
+console.log("hello")
+
+
---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,7 @@
+const number = 24
+
+if (number % 3 === 0) {
+    console.log(nomber + "!!!!!!!")
+}else {
+    console.log(number)
+}
