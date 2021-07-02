76de36838abe6d06d988a32562533170702d6121
MariKojima <pfkojimari@gmail.com>
2021-07-03 01:21:30 +0900

    Java Scriptで遊んでみた完了

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,23 @@
+
+const button = document.getElementById("button")
+
+const alertMessage = function() {
+  alert("クリックしたね")
+}
+
+button.onclick = alertMessage
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const sayHello = function() {
+  console.log("hello")
+}
+
+inputText.oninput = sayHello
+inputDate.oninput = sayHello
+
+document.onkeydown = function(e) {
+    console.log(e.key)
+  }
+  
--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -27,8 +27,11 @@ const f3 = function (n) {
 
 // 練習問題３
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply
-
+const multiply = function (x, y) {
+  return x * y
+}
 
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound
-
-
+const plusRound = function (x, y) {
+  return Math.round(x + y)
+}
