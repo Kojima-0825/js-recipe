c596617d3eebbc73856ece6f81c9c14d6a92c29a
MariKojima <pfkojimari@gmail.com>
2021-07-09 16:25:38 +0900

    [DogAPIで非同期処理]を学習しました。

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -5,7 +5,7 @@ const choice2 = document.getElementById("choice-2")
 const choice3 = document.getElementById("choice-3")
 const feedback = document.getElementById("feedback")
 
-// クイズの内容
+
 const quiz = {
   text: "この星の名前は何でしょう？",
   image: "Ganymede.jpg",
@@ -26,38 +26,36 @@ const quiz = {
   ],
 }
 
-// quiz を画面に表示する関数
+
 const reloadQuiz = function() {
-  // 問題文を表示
+
   quizText.textContent = "Q. " + quiz.text
 
-  // 画像を表示
   quizImage.src = "./images/" + quiz.image
 
-  // 選択肢（ボタン）の中身を表示
   choice1.textContent = quiz.choices[0].text
   choice2.textContent = quiz.choices[1].text
   choice3.textContent = quiz.choices[2].text
 }
 
-// choiceNumber番目の選択肢を選択
+
 const choose = function(choiceNumber) {
-  // フィードバックを表示
+
   feedback.textContent = quiz.choices[choiceNumber].feedback
 }
 
 choice1.onclick = function() {
-  // 0 番目の選択肢を選択
+
   choose(0)
 }
 choice2.onclick = function() {
-  // 1 番目の選択肢を選択
+
   choose(1)
 }
 choice3.onclick = function() {
-  // 2 番目の選択肢を選択
+
   choose(2)
 }
 
-// reloadQuiz関数 を実行して、クイズを画面に表示する
+
 reloadQuiz()
