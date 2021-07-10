d4e2a6b756d8f2e862df20caeec46716108836fd
MariKojima <pfkojimari@gmail.com>
2021-07-11 00:52:05 +0900

    カウンター発展1

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusoneButton = document.getElementById("minusone-button")
+const multypleTwo = document.getElementById("multypletwo")
 
 let count = 0
 
@@ -8,4 +10,22 @@ plusButton.onclick = function() {
 count += 1
 
 display.textContent = count
+}
+
+let number = 0
+
+minusoneButton.onclick = function() {
+
+    number -= 1
+
+    display.textContent = number
+}
+
+let figure = 1
+
+multypleTwo.onclick = function() {
+
+    figure  *= 2
+
+    display.textContent = figure
 }
