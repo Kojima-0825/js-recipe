7ef94ccfce0b33b0debdfabaddd6bf74847429fa
MariKojima <pfkojimari@gmail.com>
2021-07-25 18:02:26 +0900

    ポートフォリオひとまず完成

--- 04_ゼミ/Homepage/main.js
+++ 04_ゼミ/Homepage/main.js
@@ -15,4 +15,7 @@ function viewSlide(className, slideNo = -1)
 	imgArray[slideNo].style.opacity = 1;
 	let msec = document.getElementById('slide_speed').value;
 	setTimeout(function(){viewSlide(className, slideNo);}, msec);
+
 }
+
+
